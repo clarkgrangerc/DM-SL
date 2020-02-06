36b538eced666180dea66ff0da9b727ddef854f7
clarkgrangerc <60335117+clarkgrangerc@users.noreply.github.com>
2020-02-05 21:12:57 -0600

    Update sclass.R

--- sclass.R
+++ sclass.R
@@ -1,15 +1,10 @@
 library(mosaic)
-sclass = read.csv('C:/Users/clark/OneDrive/UTEXAS/Spring20/Data Mining/sclass.csv')
-
-# The variables involved
+sclass = read.csv('sclass.csv')
 summary(sclass)
 
 # Focus on 2 trim levels: 350 and 65 AMG
 sclass350 = subset(sclass, trim == '350')
-dim(sclass350)
-
 sclass65AMG = subset(sclass, trim == '65 AMG')
-summary(sclass65AMG)
 
 # Look at price vs mileage for each trim level
 plot(price ~ mileage, data = sclass350)
@@ -20,10 +15,13 @@ plot(price ~ mileage, data = sclass65AMG)
 library(tidyverse)
 library(FNN)
 ######Plots fot both trims
-ggplot(data = sclass350) + 
+windows()
+plot350 =ggplot(data = sclass350) + 
+  geom_point(mapping = aes(x = mileage, y = price), color='blue')
+plot350
+plot65AMG =ggplot(data = sclass65AMG) + 
   geom_point(mapping = aes(x = mileage, y = price), color='blue')  
-ggplot(data = sclass65AMG) + 
-  geom_point(mapping = aes(x = mileage, y = price), color='blue')  
+plot65AMG
 
 # Make a train-test split 350
 N350 = nrow(sclass350)
@@ -46,9 +44,9 @@ D_train350 = sclass350[train_ind350,]
 D_test350 = sclass350[-train_ind350,]
 
 # optional book-keeping step:
-# reorder the rows of the testing set by the KHOU (temperature) variable
+# reorder the rows of the testing set by the mileage variable
 # this isn't necessary, but it will allow us to make a pretty plot later
-D_test350 = arrange(D_test350, price)
+D_test350 = arrange(D_test350, mileage)
 head(D_test350)
 
 # Now separate the training and testing sets into features (X) and outcome (y)
@@ -58,38 +56,45 @@ X_test350 = select(D_test350, mileage)
 y_test350 = select(D_test350, price)
 
 #####
-# Fit a few models for 350 A
+########## K -Nearest Neighbor Model##############
 #####
-
-# linear and quadratic models
-lm350 = lm(price ~ mileage, data=D_train350)
-summary(lm350)
-names(lm350)
-plot(lm350$fitted.values)
-#lm2 = lm(COAST ~ poly(KHOU, 2), data=D_train)
-
-# KNN 10
-knn10 = knn.reg(train = X_train350, test = X_test350, y = y_train350, k=10)
-summary(knn10)
-names(knn10)
-
-#####
-# Compare the models by RMSE_out
-#####
-
 # define a helper function for calculating RMSE
 rmse = function(y, ypred) {
   sqrt(mean(data.matrix((y-ypred)^2)))
 }
 
-ypred_lm350 = predict(lm350, X_test350)
-#ypred_lm2 = predict(lm2, X_test)
-ypred_knn10 = knn10$pred
+knn_output <- data.frame(K=c(),rmse=c())
 
-rmse(y_test350, ypred_lm350)
-#rmse(y_test, ypred_lm2)
-rmse(y_test350, ypred_knn10)
+for(i in c(3:nrow(X_train350))){
+  knn_Gen = knn.reg(train = X_train350, test = X_test350, y = y_train350, k=i)
+  ypred_knn350 = knn_Gen$pred
+  knn_rmse =rmse(y_test350,ypred_knn350)
+  knn_output = rbind(knn_output,c(i,knn_rmse))
+}
+colnames(knn_output) <- c("k","RMSE")
+k_min = knn_output$k[which.min(knn_output$RMSE)]
 
+###########Graph rsme vs k ################
+plot350_k= ggplot(data = knn_output)+
+  geom_line(aes(x = k, y = RMSE))+
+  geom_line(aes(x = k_min, y = RMSE), col = "red")
+plot350_k
 
+# Running the best Knn model 
+#####
 
+knn_best = knn.reg(train = X_train350, test = X_test350, y = y_train350, k=k_min)
+ypred_knn350_best=knn_best$pred
+rmse(y_test350, ypred_knn350_best)
+
+####
+# plot the fit
+####
+# attach the predictions to the test data frame
+D_test350$ypred_knn350_best = ypred_knn350_best
+
+p_test350 = ggplot(data = D_test350) + 
+  geom_point(mapping = aes(x = mileage, y = price), color='lightgrey')+ 
+  geom_path(aes(x = mileage, y = ypred_knn350_best), color='red')
+p_test350 
 
